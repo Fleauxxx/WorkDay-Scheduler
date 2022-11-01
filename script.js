66d29aa79a81dd01924545018dadb22bbc970c72
Floyd Hudson <hudsonfloyd@yahoo.com>
2022-11-01 13:45:51 -0500

    added more javascript for my hour classes to show up

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMM Do, YYYY"));
+var saveBtnEl = $('saveBtn');
+
+
+$('#time').each(function() {
+    var timeId = parseInt($(this).attr('#timeId'));
+    var currentHr = moment().hours();
+    if (timeId < currentHr) {
+        $(this).addClass('past');
+    } else if (timeId === currentHr) {
+        $(this).removeClass('past');
+        $(this).removeClass('future');
+        $(this).addClass('present');
+    } else {
+        $(this).removeClass('past');
+        $(this).removeClass('present');
+        $(this).addClass('future');
+
+    }
+    
+});
 
